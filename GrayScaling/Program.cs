26eebf6f456123587ded927e59c47227bdbb8d2a
artem4090@gmail.com <artem4090@gmail.com>
2022-08-19 21:55:26 +0300

    ResizeBitmap done

--- GrayScaling/Program.cs
+++ GrayScaling/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 
 string ImgPath = "E:\\School\\Master\\Diploma\\fishTEST";
@@ -32,17 +33,17 @@ void transform(string FileName1, long incre)
             ResizeBitmap(image, 224, 224).Save(FileName1);
         }
     }
-    using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
+   /* using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
     {
         using (Bitmap? image = new Bitmap(ms))
         {
             FlipBitmapHorizontal(image).Save(Path.GetDirectoryName(FileName1)
-                     + Path.DirectorySeparatorChar
-                     + Path.GetFileNameWithoutExtension(FileName1)
-                     + incre.ToString()
-                     + Path.GetExtension(FileName1));
+                                             + Path.DirectorySeparatorChar
+                                             + Path.GetFileNameWithoutExtension(FileName1)
+                                             + incre.ToString()
+                                             + Path.GetExtension(FileName1));
         }
-    }
+    }*/
 
 }
 
@@ -88,21 +89,61 @@ void GaussianNoise(Bitmap original)
 
 }
 
-Bitmap ResizeBitmap(Bitmap original, int width, int height)
+/*Bitmap ResizeBitmap(Bitmap original, int width, int height)
 {
     Bitmap result = new Bitmap(width, height);
     using (Graphics g = Graphics.FromImage(result))
     {
-        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+        g.InterpolationMode = InterpolationMode.NearestNeighbor;
         g.DrawImage(original, 0, 0, width, height);
     }
     return result;
-}
+}*/
 
+
+Bitmap ResizeBitmap(Bitmap original, int canvasWidth, int canvasHeight)
+{
+    Bitmap result = new Bitmap(canvasWidth, canvasHeight);
+    using (Graphics g = Graphics.FromImage(result))
+    {
+        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        g.SmoothingMode = SmoothingMode.HighQuality;
+        g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+        g.CompositingQuality = CompositingQuality.HighQuality;
+
+        // Figure out the ratio
+        double ratioX = (double)canvasWidth / original.Width;
+        double ratioY = (double)canvasHeight / original.Height;
+        // use whichever multiplier is smaller
+        double ratio = ratioX < ratioY ? ratioX : ratioY;
+
+        // now we can get the new height and width
+        int newHeight = Convert.ToInt32(original.Height * ratio);
+        int newWidth = Convert.ToInt32(original.Width * ratio);
+
+        // Now calculate the X,Y position of the upper-left corner 
+        // (one of these will always be zero)
+        int posX = Convert.ToInt32((canvasWidth - (original.Width * ratio)) / 2);
+        int posY = Convert.ToInt32((canvasHeight - (original.Height * ratio)) / 2);
+
+        g.Clear(Color.Transparent); // Transparent padding
+        g.DrawImage(original, posX, posY, newWidth, newHeight);
+
+        /* ------------- end new code ---------------- */
+
+       /* ImageCodecInfo[] info = ImageCodecInfo.GetImageEncoders();
+        EncoderParameters encoderParameters;
+        encoderParameters = new EncoderParameters(1);
+        encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 100L);
+        thumbnail.Save(path + newWidth + "." + originalFilename, info[1], encoderParameters);*/
+    }
+    return result;
+}
+/*
 static void Swap<T>(ref T x, ref T y)
 {
     var temp = x; x = y; y = temp;
-}
+}*/
 /*
 public class DirectBitmap : IDisposable
 {
