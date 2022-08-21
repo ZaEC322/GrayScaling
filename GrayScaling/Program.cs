2741d6e74fd1907b45d7c34b97cb94ee01ba69ea
artem4090@gmail.com <artem4090@gmail.com>
2022-08-22 00:20:16 +0300

    finshed resize and rotate

--- GrayScaling/Program.cs
+++ GrayScaling/Program.cs
@@ -2,66 +2,105 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 string ImgPath = "E:\\School\\Master\\Diploma\\fishTEST";
 
+
+
+Console.WriteLine("ResizeBitmap + FlipBitmapHorizontal");
 var paths = Directory.GetFiles(ImgPath, "*.png*", SearchOption.AllDirectories)
                 .ToList();
-long temp = 0;
 foreach (var item in paths)
 {
     Console.WriteLine(item);
-    transform(item, temp);
-    Console.WriteLine("done");
-    temp++;
+    transform1(item);
+}
+Console.WriteLine("FlipBitmapVertical");
+paths = Directory.GetFiles(ImgPath, "*.png*", SearchOption.AllDirectories)
+                .ToList();
+foreach (var item in paths)
+{
+    Console.WriteLine(item);
+    transform2(item);
+}
+Console.WriteLine("RotationOnDegree");
+
+paths = Directory.GetFiles(ImgPath, "*.png*", SearchOption.AllDirectories)
+                .ToList();
+foreach (var item in paths)
+{
+    Console.WriteLine(item);
+    transform3(item);
 }
 
-
-//
-// ПРИДУМАТЬ КАК ДЕЛАТЬ НАЗВАНИЯ ДЛЯ НОВЫХ ФАЙЛОВ
-// 
-//
-void transform(string FileName1, long incre)
+void transform1(string FileName1)
 {
     using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
     {
         using (Bitmap? image = new Bitmap(ms))
         {
-            //ToWhiteBlack(image);
-
             File.Delete(FileName1.ToString());
             ResizeBitmap(image, 224, 224).Save(FileName1);
         }
     }
-   /* using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
+    using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
     {
         using (Bitmap? image = new Bitmap(ms))
         {
             FlipBitmapHorizontal(image).Save(Path.GetDirectoryName(FileName1)
                                              + Path.DirectorySeparatorChar
                                              + Path.GetFileNameWithoutExtension(FileName1)
-                                             + incre.ToString()
+                                             + "_FlipedHorizontal"
                                              + Path.GetExtension(FileName1));
         }
-    }*/
+    }
+
 
 }
 
 
-
-/*void ToWhiteBlack(Bitmap original)
+void transform2(string FileName1)
 {
-    for (var i = 0; i < original.Width; i++)
+
+
+    using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
     {
-        for (var j = 0; j < original.Height; j++)
+        using (Bitmap? image = new Bitmap(ms))
         {
-            var originalColor = original.GetPixel(i, j);
-            var grayScale = (int)((originalColor.R*0.3) + (originalColor.G*0.59) + (originalColor.B*0.11));
-            var corEmEscalaDeCinza = Color.FromArgb(originalColor.A, grayScale, grayScale, grayScale);
-            original.SetPixel(i, j, corEmEscalaDeCinza);
+            FlipBitmapVertical(image).Save(Path.GetDirectoryName(FileName1)
+                                             + Path.DirectorySeparatorChar
+                                             + Path.GetFileNameWithoutExtension(FileName1)
+                                             + "_FlipVertical"
+                                             + Path.GetExtension(FileName1));
         }
     }
-}*/
+}
+
+
+void transform3(string FileName1)
+{
+
+
+    using (var ms = new MemoryStream(File.ReadAllBytes(FileName1)))
+    {
+        using (Bitmap? image = new Bitmap(ms))
+        {
+            for (int i = 15; i < 180; i = i + 15)
+            {
+                RotationOnDegree(image, i).Save(Path.GetDirectoryName(FileName1)
+                                             + Path.DirectorySeparatorChar
+                                             + Path.GetFileNameWithoutExtension(FileName1)
+                                             + "_RotationOnDegree" + i
+                                             + Path.GetExtension(FileName1));
+            }
+           
+
+        }
+    }
+
+}
+
 
 Bitmap FlipBitmapHorizontal(Bitmap original)
 {
@@ -69,36 +108,30 @@ Bitmap FlipBitmapHorizontal(Bitmap original)
     return original;
 }
 
-void RotationOnDegree(Bitmap original)
+Bitmap FlipBitmapVertical(Bitmap original)
 {
-
+    original.RotateFlip(RotateFlipType.RotateNoneFlipY);
+    return original;
 }
 
-void Scale(Bitmap original)
+Bitmap RotationOnDegree(Bitmap bmp, float angle)
 {
-
-}
-
-void Crop(Bitmap original)
-{
-
-}
-
-void GaussianNoise(Bitmap original)
-{
-
-}
-
-/*Bitmap ResizeBitmap(Bitmap original, int width, int height)
-{
-    Bitmap result = new Bitmap(width, height);
-    using (Graphics g = Graphics.FromImage(result))
+    //create a new empty bitmap to hold rotated image
+    Bitmap returnBitmap = new Bitmap(bmp.Width, bmp.Height);
+    //make a graphics object from the empty bitmap
+    using (Graphics g = Graphics.FromImage(returnBitmap))
     {
-        g.InterpolationMode = InterpolationMode.NearestNeighbor;
-        g.DrawImage(original, 0, 0, width, height);
+        //move rotation point to center of image
+        g.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);
+        //rotate
+        g.RotateTransform(angle);
+        //move image back
+        g.TranslateTransform(-(float)bmp.Width / 2, -(float)bmp.Height / 2);
+        //draw passed in image onto graphics object
+        g.DrawImage(bmp, new Point(0, 0));
     }
-    return result;
-}*/
+    return returnBitmap;
+}
 
 
 Bitmap ResizeBitmap(Bitmap original, int canvasWidth, int canvasHeight)
@@ -130,62 +163,6 @@ Bitmap ResizeBitmap(Bitmap original, int canvasWidth, int canvasHeight)
         g.DrawImage(original, posX, posY, newWidth, newHeight);
 
         /* ------------- end new code ---------------- */
-
-       /* ImageCodecInfo[] info = ImageCodecInfo.GetImageEncoders();
-        EncoderParameters encoderParameters;
-        encoderParameters = new EncoderParameters(1);
-        encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 100L);
-        thumbnail.Save(path + newWidth + "." + originalFilename, info[1], encoderParameters);*/
     }
     return result;
 }
-/*
-static void Swap<T>(ref T x, ref T y)
-{
-    var temp = x; x = y; y = temp;
-}*/
-/*
-public class DirectBitmap : IDisposable
-{
-    public Bitmap Bitmap { get; private set; }
-    public Int32[] Bits { get; private set; }
-    public bool Disposed { get; private set; }
-    public int Height { get; private set; }
-    public int Width { get; private set; }
-
-    protected GCHandle BitsHandle { get; private set; }
-
-    public DirectBitmap(int width, int height)
-    {
-        Width = width;
-        Height = height;
-        Bits = new Int32[width * height];
-        BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
-        Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
-    }
-
-    public void SetPixel(int x, int y, Color colour)
-    {
-        int index = x + (y * Width);
-        int col = colour.ToArgb();
-
-        Bits[index] = col;
-    }
-
-    public Color GetPixel(int x, int y)
-    {
-        int index = x + (y * Width);
-        int col = Bits[index];
-        Color result = Color.FromArgb(col);
-
-        return result;
-    }
-
-    public void Dispose()
-    {
-        if (Disposed) return;
-        Disposed = true;
-        Bitmap.Dispose();
-        BitsHandle.Free();
-    }
-}*/
